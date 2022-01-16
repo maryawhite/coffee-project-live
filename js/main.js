25ddb9c14c948d5a11cfc4fb68127fe52760ae72
Mary White <mary.a.white28@gmail.com>
2022-01-15 21:50:34 -0600

    flipped the form and displayed coffees divs and made the dispalyed coffees a single column on xs screens for a more mobile friendly look

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<div class="coffee row align-items-center col-6 p-2">';
+    var html = '<div class="coffee col-12 align-items-center col-md-6 p-2 flex-wrap">';
     html += '<div class="d-none">' + coffee.id + '</div>';
-    html += '<h3 class="coffee-name">' + coffee.name + '</h3>';
+    html += '<h3 class="coffee-name mb-0">' + coffee.name + '</h3>';
     html += '<div class="ml-2 coffee-roast">' + coffee.roast + '</div>';
     html += '</div>';
 
